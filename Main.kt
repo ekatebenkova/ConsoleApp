aad6a6ac200ffad2ff0bb91b47838cda288d38ab
Ekaterina Tebenkova <biruta78@gmail.com>
2023-02-06 20:01:25 +0500

    created Kotlin Console App

--- Main.kt
+++ Main.kt
@@ -1,3 +1,12 @@
-fun main(args: Array<String>) {
-
+fun main() {
+   do {
+        val inputStr = readLine()
+        when (inputStr) {
+            "/start" -> println("Hello, Guest!")
+            "/help" -> println("/start - program start, \n/end - end of program")
+            "/end" -> break
+            else -> println("Program is unable to process the request, call /help")
+        }
+   } while(true)
+    println("Goodbye, Guest")
 }
