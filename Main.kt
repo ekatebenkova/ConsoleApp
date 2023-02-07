008bafe455725322f34bf478acb981f988e668b6
Ekaterina Tebenkova <biruta78@gmail.com>
2023-02-07 10:27:52 +0500

    fix

--- Main.kt
+++ Main.kt
@@ -5,7 +5,7 @@ fun main() {
             "/start" -> println("Hello, Guest!")
             "/help" -> println("/start - programm start, \n/end - end of programm")
             "/end" -> break
-            else -> println("Program is unable to process the request, call /help")
+            else -> println("Programm is unable to process the request, call /help")
         }
    } while(true)
     println("Goodbye, Guest")
